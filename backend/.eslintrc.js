744d1363b8e47d336c015e34fdb787ca0d72d224
Tommaso Bianchi <tommaso.bianchi@gmail.com>
2023-05-21 18:09:05 +0000

    Update backend .eslintrc file to increase max-len

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,15 +1,16 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'commonjs': true,
-    'es2021': true,
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
   },
-  'extends': 'google',
-  'overrides': [
+  extends: 'google',
+  overrides: [
   ],
-  'parserOptions': {
-    'ecmaVersion': 'latest',
+  parserOptions: {
+    ecmaVersion: 'latest',
   },
-  'rules': {
+  rules: {
+    'max-len': ['warn', {code: 120}],
   },
 };
