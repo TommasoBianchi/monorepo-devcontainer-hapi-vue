c12133e7660e45b8e3e5e81a1a91da0bd578a82f
Tommaso Bianchi <tommaso.bianchi@polimi.it>
2023-05-28 18:51:20 +0000

    Fully integrate adminJS with schwifty models

--- backend/lib/plugins/@adminjs.hapi.js
+++ backend/lib/plugins/@adminjs.hapi.js
@@ -1,9 +1,16 @@
 
 'use strict';
 
+const Manifest = require('../../server/manifest');
 const {models: usersModels} = require('../../users-plugin');
+const Knex = require('knex');
 
 module.exports = async (server, options) => {
+  // TODO: find a better way to load and setup knex options
+  const knexOptions = Manifest.get('/register/plugins', process.env)
+      .find(({plugin}) => plugin == '@hapipal/schwifty').options.knex;
+  const knex = new Knex(knexOptions);
+
   const AdminJS = await import('adminjs').then((module) => module.default);
   const AdminJSHapi = await import('@adminjs/hapi').then((module) => module.default);
   const AdminJSObjection = await import('@adminjs/objection');
@@ -16,7 +23,7 @@ module.exports = async (server, options) => {
   return {
     plugin: AdminJSHapi,
     options: {
-      resources: [...Object.values(usersModels)],
+      resources: [...Object.values(usersModels).map((model) => model.bindKnex(knex))],
       rootPath: '/admin',
       auth: {
         isSecure: process.env.NODE_ENV === 'production',
