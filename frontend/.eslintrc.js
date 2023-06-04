cd5d685017c25313257e85d5aee2252987b5eea3
Tommaso Bianchi <tommaso.bianchi@gmail.com>
2023-06-04 10:09:10 +0000

    Setup eslint+prettier for frontend

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,24 +1,23 @@
+require("@rushstack/eslint-patch/modern-module-resolution");
+
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
+  env: {
+    browser: true,
+    es2021: true,
   },
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'google',
-  ],
-  'overrides': [
-  ],
-  'parserOptions': {
-    'ecmaVersion': 6,
-    'sourceType': 'module',
-    'ecmaFeatures': {
-      'modules': true,
+  extends: ["plugin:vue/vue3-recommended", "@vue/eslint-config-prettier"],
+  overrides: [],
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module",
+    ecmaFeatures: {
+      modules: true,
     },
   },
-  'plugins': [
-    'vue',
-  ],
-  'rules': {
+  plugins: ["vue"],
+  rules: {
+    // Increase max length of code lines
+    "max-len": ["warn", { code: 120 }],
   },
 };
